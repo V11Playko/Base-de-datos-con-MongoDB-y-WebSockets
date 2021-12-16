d4ff1cf75a1f4aa7581b902ce4bf8f96e6e42617
V11Playko <heinnervega20@gmail.com>
2021-12-16 08:11:29 -0500

    MongoDB: Almacenar y leer datos

--- components/message/store.js
+++ components/message/store.js
@@ -1,14 +1,34 @@
-const list = [];
+const db = require('mongoose');
+const Model = require('./model');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const uri =
+process.env.DB_CONNECT;
+
+db.Promise = global.Promise;
+
+db.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, dbName: 'telegrom' })
+  .then(() => console.log('[db] Conectada con éxito'))
+  .catch(err => console.error('[db]', err));
 
 function addMessage(message) {
-    list.push(message);
+  const myMessage = new Model(message);
+  myMessage.save();
 }
 
-function getMessage() {
-    return list;
+async function getMessages() {
+  // return list;
+  const messages = await Model.find();
+  return messages; 
 }
 
 module.exports = {
-    add: addMessage,
-    list: getMessage,
-}
+  add: addMessage,
+  list: getMessages,
+  //get
+  // update
+  // delete
+};
+
+console.log(process.env.DB_USER) //root
