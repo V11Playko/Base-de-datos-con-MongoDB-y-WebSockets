3e1a41f05b0c457a00164c8f4580788f82ec1441
V11Playko <heinnervega20@gmail.com>
2021-12-17 07:06:14 -0500

    Probando los componentes

--- components/message/store.js
+++ components/message/store.js
@@ -1,49 +1,49 @@
 const Model = require('./model');
 
 function addMessage(message) {
-  const myMessage = new Model(message);
-  myMessage.save();
+    const myMessage = new Model(message);
+    myMessage.save();
 }
 
-async function getMessages(filterUser) {
-  return new Promise((resolve, reject) => {
-    let filter = {};
-    if (filterUser !== null) {
-      filter = {user: filterUser};
-    }
-    Model.find(filter)
-      .populate('user')
-      .exec((error, populated) => {
-        if (error) {
-          reject(error);
+async function getMessages(filterChat) {
+    return new Promise((resolve, reject) => {
+        let filter = {};
+        if (filterChat !== null) {
+            filter = { chat: filterChat };
         }
-        resolve(populated);
-      });
-  })
-}
+        Model.find(filter)
+            .populate('user')
+            .exec((error, populated) => {
+                if (error) {
+                    reject(error);
+                    return false;
+                }
 
-async function updateText(id, message) {
-  const foundMessage = await Model.findOne({
-    _id: id
-  });
-  foundMessage.message = message;
-
-  const newMessage = await foundMessage.save();
-  return newMessage;
+                resolve(populated);
+            });
+    })
 }
 
 function removeMessage(id) {
-  return Model.deleteOne ({
-    _id:id
-  });
+    return Model.deleteOne({
+        _id: id
+    });
+}
+
+async function updateText(id, message) {
+    const foundMessage = await Model.findOne({
+        _id: id
+    });
+
+    foundMessage.message = message;
+
+    const newMessage = await foundMessage.save();
+    return newMessage;
 }
 
 module.exports = {
-  add: addMessage,
-  list: getMessages,
-  updateText: updateText,
-  remove: removeMessage,
-  //get
-  // update
-  // delete
-};
+    add: addMessage,
+    list: getMessages,
+    updateText: updateText,
+    remove: removeMessage,
+}
